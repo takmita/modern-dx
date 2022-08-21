149721aa4e7391895ac8d94ee4fd20d5c70c42cd
mitamurat <mita@esm.co.jp>
2022-08-21 12:04:45 +0900

    web-app-dev-3-1  Vue.jsを使う

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 function doGet() {
-    return HtmlService.createTemplateFromFile('index').evaluate();
-}
+  return HtmlService.createTemplateFromFile("index").evaluate();
 
+}
 function writeSheet(e) {
-    var ss = openSheet();
-    ss.getRange("A1").setValue(e)
+  var ss = openSheet();
+  ss.getRange("A1").setValue(e)
 }
 
 function openSheet() {
-    var id = "1hSnXqpaT9qjTz5FWFWtez0TuGVQcSywFhHz8KtYiaYo";
-    var ss = SpreadsheetApp.openById(id).getSheetByName("シート1");
-    return ss;
+  var id = "1hSnXqpaT9qjTz5FWFWtez0TuGVQcSywFhHz8KtYiaYo";
+  var ss = SpreadsheetApp.openById(id).getSheetByName("シート1");
+  return ss;
 }
 
 function readSheet() {
-    var ss = openSheet();
-    var data = ss.getRange("A1").getValue();
-    return data;
+  var ss = openSheet();
+  var data = ss.getRange("A1").getValue();
+  return data;
 }
