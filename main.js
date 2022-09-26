b4915c776321a95b3645d6d37aeec97ea7d51cc7
mitamurat <mita@esm.co.jp>
2022-09-26 22:47:26 +0900

    DB読込関数を作成

--- main.js
+++ main.js
@@ -2,36 +2,40 @@ function doGet() {
   return HtmlService.createTemplateFromFile("index").evaluate();
 }
 
+const CONECTION_NAME = 'esm-gcp-study:us-central1:modern-study'; //Instance_connection_name
+const DATABASE_NAME = 'web_counter_takmita'; //database_name  
+const URL = 'jdbc:google:mysql://' + CONECTION_NAME + '/' + DATABASE_NAME;
+const USER_NAME = 'esm'; //user_name
+const PASSWORD = 'esm'; //'user_password'
+
 /**
  * Write one row of data to a table.
  */
-
 function updateOneRecord(updateCountr) {
-  const CONECTION_NAME = 'esm-gcp-study:us-central1:modern-study'; //Instance_connection_name
-  const USER_NAME = 'esm'; //user_name
-  const PASSWORD = 'esm'; //'user_password'
-  const DATABASE_NAME = 'web_counter_takmita'; //database_name
-   
-  const URL = 'jdbc:google:mysql://' + CONECTION_NAME + '/' + DATABASE_NAME;
-  const connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
- 
+  const connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);  
   let statement = connection.prepareStatement('UPDATE web_counter_takmita SET counter=?');
-
+  
   statement.setString(1, updateCountr);
   statement.executeUpdate();
-
+  
   statement.close();
   connection.close();
 }
 
-function openSheet() {
-  const ID = "1hSnXqpaT9qjTz5FWFWtez0TuGVQcSywFhHz8KtYiaYo";
-  const ss = SpreadsheetApp.openById(ID).getSheetByName("シート1");
-  return ss;
-}
+/**
+ * read all row of data to a table.
+ */
+function readFromTable() {
+  const connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
+  const statement = connection.createStatement();
+  const results = statement.executeQuery('SELECT * FROM web_counter_takmita'); 
+  
+  while (results.next()) {
+    let counter = results.getInt('counter');
+  }
+  return this.counter;
 
-function readSheet() {
-  const ss = openSheet();
-  const data = ss.getRange("A1").getValue();
-  return data;
+  results.close();
+  statement.close();
+  connection.close();
 }
